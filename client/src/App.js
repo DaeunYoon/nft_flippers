ef4e6f5159142745cb65d58a76cb5554173205e3
daeunYoon <nanuli00@naver.com>
2022-06-07 00:23:48 +0200

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
-      <h2>NFT FLIPPERS</h2>
+      <Navbar />
+      <Home />
     </>
   );
 }
